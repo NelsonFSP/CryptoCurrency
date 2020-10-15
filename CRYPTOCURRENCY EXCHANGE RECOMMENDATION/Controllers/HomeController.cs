6e7fee9cc16638c1cde6b34b045809bce8fd629e
NelsonFSP <nelson.santosdepaula@yahoo.com.br>
2020-10-15 18:35:53 -0300

    first commit with changes on home view

---
+++ CRYPTOCURRENCY EXCHANGE RECOMMENDATION/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CRYPTOCURRENCY_EXCHANGE_RECOMMENDATION.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CRYPTOCURRENCY_EXCHANGE_RECOMMENDATION.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            SelectListItem item1 = new SelectListItem()
+            {
+                Text = "Bitcoin",
+                Value = "1",
+                Selected = true
+            };
+            SelectListItem item2 = new SelectListItem()
+            {
+                Text = "Ethereum",
+                Value = "2",
+                Selected = false
+            };
+            SelectListItem item3 = new SelectListItem()
+            {
+                Text = "Classic Ethereum",
+                Value = "3",
+                Selected = false
+            };
+            SelectListItem item4 = new SelectListItem()
+            {
+                Text = "Monero",
+                Value = "4",
+                Selected = false
+            };
+            SelectListItem item5 = new SelectListItem()
+            {
+                Text = "Zcash",
+                Value = "5",
+                Selected = false
+            };
+            SelectListItem item6 = new SelectListItem()
+            {
+                Text = "Bitcoin cash",
+                Value = "6",
+                Selected = false
+            };
+            items.Add(item1);
+            items.Add(item3);
+            items.Add(item2);
+            items.Add(item3);
+            items.Add(item4);
+            items.Add(item5);
+            items.Add(item6);
+
+            ViewBag.TradeItems = items;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CRYPTOCURRENCY EXCHANGE RECOMMENDATION/Models/Trade.cs
@@ -0,0 +1,16 @@
+﻿using CRYPTOCURRENCY_EXCHANGE_RECOMMENDATION.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRYPTOCURRENCY_EXCHANGE_RECOMMENDATION.Models
+{
+    public class Trade
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
